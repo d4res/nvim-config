7d916ab676ba7de73e6acb76f4beb2644613b6be
d4res <1781117126@qq.com>
2022-09-13 13:04:16 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,10 +58,6 @@ packer.startup({
 
         use {
             'lewis6991/gitsigns.nvim',
-            config = function()
-                require('gitsigns').setup()
-            end
-            -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
         }
     end,
     config = {
